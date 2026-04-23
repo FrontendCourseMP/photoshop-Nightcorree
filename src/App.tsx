00116981a684c084b17d6f144a312c73fd72d555
Nightcore <Farmax7777@yandex.ru>
2026-04-23 19:20:46 +0300

    Init: React + TypeScript + Tailwind v4 + Lucide

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-editor-bg text-editor-text flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-editor-accent">
+        Графический редактор
+      </h1>
+      <p className="mt-4">Tailwind v4 успешно подключен!</p>
+    </div>
+  )
+}
+
+export default App
